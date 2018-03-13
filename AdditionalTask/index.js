070213a3b16d782849296bd46fb268fa7741fe48
Bratok3000 <bratok3000@gmail.com>
2018-03-13 11:45:11 +0200

    Additional Task complete

--- AdditionalTask/index.js
+++ AdditionalTask/index.js
@@ -1,18 +1,28 @@
-window.addEventListener("submit", function(e){
+// document.forms[0].addEventListener("submit", function(e){
+// for(var i=0; i< document.forms[0].elements.length; i++){
+//     document.forms[0].elements[i].onfocus=function(){
+//         this.style.border = "1px solid red";
+//     } 
+//     document.forms[0].elements[i].onblur = function(){
+//         this.style.border = "1px dotted green";
+//     }
+// }
 
-})
-
-for(var i=0; i< document.forms[0].elements.length; i++){
-    document.forms[0].elements[i].onfocus=function(){
-        this.style.border = "1px solid red";
-    } 
-    document.forms[0].elements[i].onblur = function(){
-        this.style.border = "1px dotted green";
-    }
-}
 pas1 = document.forms[0].passw;
 pas2 = document.forms[0].spassw;
-
-if(pas1.value != "" && pas2.value != "" && pas1.value == pas2.value){
-    alert("Everithing alright");
+msgerror = document.getElementById("pasMsg")
+pas2.onblur = function () {
+    if (pas1.value != "" && pas2.value != "" && pas1.value == pas2.value) {
+        // if(pas1.value == pas2.value){
+        // alert("Everything alright");  
+        msgerror.innerHTML = "Passwords are equal";
+    }
+    else {
+        msgerror.innerHTML = "Passwords are not equal or wasn't entered";
+        // alert("passwords are not equal");
+    }
 }
+
+
+// })
+
