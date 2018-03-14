864bf4dbe59254573858b9213d6d63a24db7d932
Bratok3000 <Bratok3000@gmail.com>
2018-03-14 23:24:31 +0200

    homework1

--- Homework1/index.js
+++ Homework1/index.js
@@ -15,17 +15,40 @@ for (var i = 0; i < size.length; i++) {
     // if(size[i].name == "size"){
     size[i].onchange = function (e) {
         if (e.target.value == "15") {
-            summa += parseInt(e.target.value);
+            summa = parseInt(e.target.value);
         }
         else if (e.target.value == "20") {
-            summa += parseInt(e.target.value);
-            price.innerHTML = summa;
+            summa = parseInt(e.target.value);
         }
         else {
-            summa += parseInt(e.target.value);
+            summa = parseInt(e.target.value);
+           
         }
+        price.innerHTML = summa;
     }
 }
-// }
+
+for(var i = 0; i< ingredients.length; i++){
+    ingredients[i].onchange = function(e){
+        if(e.target.value === "10"){
+            if(e.target.checked == true){
+                summa += 10;
+                alert(summa);
+            }
+            else{
+                summa -= 10;
+            }
+        }
+        else if(e.target.value == "15"){
+            if(e.target.checked ==true){
+                summa += 15;
+            }
+            else{
+                summa -=15;
+            }
+        }
+        price.innerHTML = summa;
+    }
+}
 
 price.innerHTML = summa;
